43d013cc5e0f0cb2e44821e3d0c90ad1fccffaf9
negara999 <negara.sergiu2@gmail.com>
2023-04-15 23:13:05 +0300

    module3-task1

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+import gulp from "gulp";
+import plumber from "gulp-plumber";
+import sass from "gulp-dart-sass";
+import postcss from "gulp-postcss";
+import autoprefixer from "autoprefixer";
+import browser from "browser-sync";
+// Styles
+
+export const styles = () => {
+  return gulp
+    .src("source/scss/style.scss", { sourcemaps: true })
+    .pipe(plumber())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(gulp.dest("source/css", { sourcemaps: "." }))
+    .pipe(browser.stream());
+};
+
+// Server
+
+const server = (done) => {
+  browser.init({
+    server: {
+      baseDir: "source",
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+};
+
+// Watcher
+
+const watcher = () => {
+  gulp.watch("source/scss/**/*.scss", gulp.series(styles));
+  gulp.watch("source/*.html").on("change", browser.reload);
+};
+
+export default gulp.series(styles, server, watcher);
